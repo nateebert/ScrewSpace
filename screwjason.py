0540270c01a37c22ea067c4a21ed7b147ec0e4e6
nateebert <60366036+nateebert@users.noreply.github.com>
2020-01-30 15:35:34 -0500

    Update screwjason.py

--- screwjason.py
+++ screwjason.py
@@ -1,43 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 30 15:13:15 2020
-
-@author: Nate
-"""
-
 #imports
 import os
 import sys
 import json
-import random
 import requests
-import time
 from flask import Flask, request
+from random import random
 
 #define our flask app
 app = Flask(__name__)
 
-#Method will automatically execute when our endpoint receives a POST call
-@app.route('/', methods=['POST'])
-def msg_received_from_group():
+#logging function to help debug
+def log(msg):
+  print(str(msg))
+  sys.stdout.flush()
 
-  #Format the data we receive as a JSON
-  data = request.get_json()
-  log('{}'.format(data))
-  
-  #Check the text of the message sent to the chat to see if it matches our command word
-  if data['text'].lower() == "!test":
-    send_msg("Hello World!")
-	
-
-  elif data['text'].lower() == "!testpic":
-    send_msg_pic("Hello World!","https://i.groupme.com/1024x1024.jpeg.d733d6de5c36462f8d1cb67e3191b618")
-	
-	
-
-  return "ok", 200
-
- 
 #Sends a message to the chat that the bot originates from
 def send_msg(msg):
 
@@ -50,28 +26,29 @@ def send_msg(msg):
         
   request = requests.post(url=url, data=data)
 
-#sends a picture and a message to the chat
-#Picture URL must be registered with GroupMe first
-def send_msg_pic(msg, picURL):
+#Method will automatically execute when our endpoint receives a POST call
+@app.route('/', methods=['POST'])
+def msg_received_from_group():
+  #Format the data we receive as a JSON
+  data = request.get_json()
+  log('{}'.format(data))
+  
+  #Check the text of the message sent to the chat to see if it matches our command word
+  if data['name'].lower() == "nate ebert" or data['user_id'] == '40743743':
+    n = random()
+    if n<=0.25:
+      send_msg("stfu you bitch")
+    elif n<=0.5:
+      send_msg("go to hell skank")
+    elif n<=0.75:
+      send_msg("eat shit and die")
+    else:
+      send_msg("go. fuck. yourself.")
 
-  url  = 'https://api.groupme.com/v3/bots/post'
-
-  data ={
-  'bot_id' : os.getenv('GROUPME_BOT_ID'),
-  'text'   : msg,
-  "attachments" : [
-    {
-      "type"  : "image",
-      "url"   : picURL
-    }
-  ],
-  'picture_url': picURL
-  }
-
-  request = requests.post(url=url, data=data)
 
 
-#logging function to help debug
-def log(msg):
-  print(str(msg))
-  sys.stdout.flush()
+	
+	
+	
+
+  return "ok", 200
